7f6b5277ccc14d6366a929a1e2f23fc96cfc9a42
Raushan <raushankp75@gmail.com>
2023-07-05 22:14:01 +0530

    Connected backend to mongodb atlas database

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+
+const notes = require('./data/notes')
 
 
 
 const app = express()
 dotenv.config()
+connectDB();
+
+
+// MIDDLEWARE
+app.use(cors())
 
 
 
 app.get('/', (req, res) => {
     res.send('Server is running')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes)
 })
 
 const PORT = process.env.PORT || 8000
